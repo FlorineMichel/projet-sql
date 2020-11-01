b0a398df0636d988f9405359d1a9454ee7e1189c
qmoriceau <quentin.moriceau@ipilyon.net>
2020-11-01 16:45:30 +0100

    2nd push maj base

--- base.sql
+++ base.sql
@@ -1,14 +1,32 @@
 CREATE DATABASE IF NOT EXISTS pieces;
+
 CREATE TABLE work   (
                     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                     title VARCHAR(255),
                     alt_title VARCHAR(255),
                     release_date INT,
                     ending_date INT,
-                    idstudio,
-                    langue,
+                    studio,
+                    idlangue,
                     categorie,
                     description VARCHAR(255));
+
+CREATE TABLE anime (
+                            id.oeuvre,
+                            nbr_total_ep INT NOT NULL,
+                            nbr_released_ep INT AUTO_INCREMENT
+)
+
+CREATE TABLE film  (
+                    id.oeuvre,
+                    duration INT NOT NULL
+)
+
+CREATE TABLE manga  (
+                    id.oeuvre,
+                    nbr_tome INT AUTO_INCREMENT,
+                    nbr_chap INT AUTO_INCREMENT
+)
 
 CREATE TABLE users  (
                     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -26,5 +44,6 @@ CREATE TABLE artist (
                     firstname VARCAHR(255),
                     birthdate INT,
                     deathdate INT,
+                    sex VARCHAR(255),
                     idlangue
 );
