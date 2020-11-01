f251d8395f744d88ced1ddaabda24854d171b757
Florine Michel <florinemichel@orange.fr>
2020-11-01 19:10:02 +0100

    improve base.sql

--- base.sql
+++ base.sql
@@ -2,86 +2,110 @@ CREATE DATABASE IF NOT EXISTS pieces;
 
 CREATE TABLE work       (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        title VARCHAR(255),
+                        title VARCHAR(255) NOT NULL,
                         alt_title VARCHAR(255),
-                        release_date INT,
+                        release_date INT NOT NULL,
                         ending_date INT,
-                        studio ENUM('studio1', 'studio2', 'studio3'),
-                        idlangue,
-                        categorie ENUM('cat1', 'cat2', 'cat3'),
-                        description VARCHAR(255));
-
-CREATE TABLE anime      (
-                        id.oeuvre,
-                        nbr_total_ep INT NOT NULL,
-                        nbr_released_ep INT AUTO_INCREMENT
+                        studio ENUM('studio1', 'studio2', 'studio3') NOT NULL,
+                        categorie ENUM('cat1', 'cat2', 'cat3') NOT NULL,
+                        description VARCHAR(255),
+                        id_language INT NOT NULL,
+                        FOREIGN KEY (idlanguage) REFERENCES language (id)
 );
 
-CREATE TABLE film       (
-                        id.oeuvre,
+CREATE TABLE anime      (
+                        id_work INT,
+                        nbr_total_ep INT NOT NULL
+);
+
+CREATE TABLE movie       (
+                        id_work INT,
                         duration INT NOT NULL
 );
 
 CREATE TABLE manga      (
-                        id.oeuvre,
-                        nbr_tome INT AUTO_INCREMENT,
-                        nbr_chap INT AUTO_INCREMENT
+                        id_work INT,
+                        nbr_volume INT NOT NULL,
+                        nbr_chap INT NOT NULL
 );
 
 CREATE TABLE users      (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        pseudo VARCHAR(255),
-                        password VARCHAR(255),
-                        email VARCHAR(255),
+                        pseudo VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
                         birthdate INT,
-                        sex VARCHAR(255),
-                        idlangue
+                        gender VARCHAR(255),
+                        id_language INT,
+                        FOREIGN KEY (id_language) REFERENCES language (id)
 );
 
 CREATE TABLE artist     (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        lastname VARCHAR(255),
-                        firstname VARCAHR(255),
+                        lastname VARCHAR(255) NOT NULL,
+                        firstname VARCHAR(255) NOT NULL,
                         birthdate INT,
                         deathdate INT,
-                        sex VARCHAR(255),
-                        idlangue
+                        gender VARCHAR(255),
+                        id_language INT NOT NULL,
+                        FOREIGN KEY (id_language) REFERENCES language (id)
 );
 
 CREATE TABLE episode    (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        id.anime,
-                        title VARCHAR(255),
-                        nbr_episode INT,
-                        id.langue
+                        nbr_episode INT NOT NULL,
+                        title VARCHAR(255) NOT NULL,
+                        id_language INT NOT NULL,
+                        id_anime INT NOT NULL, 
+                        FOREIGN KEY (id_language) REFERENCES language (id),
+                        FOREIGN KEY (id_anime) REFERENCES anime (id)
 );
 
-CREATE TABLE chapitre   (
+CREATE TABLE chapter   (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        id.manga,
-                        title VARCHAR(255),
-                        nbr_chapitre INT,
-                        id.manga,
-                        id.langue
+                        nbr_chapter INT NOT NULL,
+                        title VARCHAR(255) NOT NULL,
+                        id_manga INT NOT NULL,
+                        id_language INT NOT NULL,
+                        FOREIGN KEY (id_language) REFERENCES language (id),
+                        FOREIGN KEY (id_manga) REFERENCES manga (id)
 );
 
-CREATE TABLE langue     (
+CREATE TABLE language     (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        nom VARCHAR(255)
+                        nom VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE genre      (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        libelle VARCHAR(255)
+                        libelle VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE theme      (
                         id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                        libelle VARCHAR(255)
+                        libelle VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE note       (
-                        id.oeuvre,
-                        id.user,
-                        note INT 
+CREATE TABLE rating       (
+                        id_work INT PRIMARY KEY NOT NULL,
+                        id_user INT PRIMARY KEY NOT NULL,
+                        rating INT NOT NULL /*limité à 10*/,
+                        FOREIGN KEY (id_work) REFERENCES work (id),
+                        FOREIGN KEY (id_user) REFERENCES user (id)
+
+);
+CREATE TABLE genreWork	(
+						id_work INT NOT NULL,
+						id_genre INT NOT NULL,
+						PRIMARY KEY (id_work, id_genre),
+						FOREIGN KEY (id_work) REFERENCES work (id),
+						FOREIGN KEY (id_genre) REFERENCES genre (id)
+);
+
+CREATE TABLE themeWork	(
+						id_work INT NOT NULL,
+						id_theme INT NOT NULL,
+						PRIMARY KEY (id_work, id_theme),
+						FOREIGN KEY (id_work) REFERENCES work (id),
+						FOREIGN KEY (id_theme) REFERENCES theme (id)
 );
