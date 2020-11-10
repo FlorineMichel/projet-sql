b5188227cf2801dd45b8a66f45fa08db5e137d6b
qmoriceau <quentin.moriceau@ipilyon.net>
2020-11-10 12:14:00 +0100

    maj birthdate users

--- base.sql
+++ base.sql
@@ -42,7 +42,6 @@ CREATE TABLE users      (
                         nickname VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL,
                         email VARCHAR(255) NOT NULL,
-                        birthdate INT,
                         gender ENUM('male', 'female', 'unknown'),
                         id_language INT,
                         FOREIGN KEY (id_language) REFERENCES language (id)
